6f423400286b6e2adfb18b962635c5c476ca70f8
heymonicakay <hey.monicakay@gmail.com>
2021-01-27 02:41:01 -0600

    refining projects and experience page

--- assets/js/experience.js
+++ assets/js/experience.js
@@ -1,61 +1,58 @@
 //  Work experience cards
-
-
 const experiencecards = document.querySelector(".experience-cards");
+
 const exp = [
   {
     title: "Full-Stack Software Developer",
     cardImage: "assets/images/experience-page/nsslogo.png",
     place: "Nashville Software School",
     time: "(July, 2020 - January 2021)",
-    desp: "Intensive six-month, full-time software development bootcamp with immersive, lab-based learning in Full-Stack Development, with a focus on HTML5, CSS3, JavaScript, React, SQL, and Python.<li>Experience with Scrum process and agile workflow</li><li>Used Git & GitHub for version control.</li><li>Experience in a full-time remote environment</li>",
+    desc: "Intensive six-month, full-time software development bootcamp with immersive, lab-based learning in Full-Stack Development, with a focus on HTML5, CSS3, JavaScript, React, SQL, and Python.<li>Experience with Scrum process and agile workflow</li><li>Used Git & GitHub for version control.</li><li>Experience in a full-time remote environment</li>",
   },
   {
     title: "Denials Specialist",
     cardImage: "assets/images/experience-page/kemby.jpeg",
     place: "Kemberton",
     time: "(2017 - 2020)",
-    desp: "Healthcare Company specializing in Patient and Provider advocacy through complex claims resolution.<li>Wrote legal appeals on behalf of healthcare Providers & Patients.</li><li>Identified repetitive tasks, and wrote macros to automate them.</li><li>Conducted daily research on current insurance policies.</li>",
+    desc: "Healthcare Company specializing in Patient and Provider advocacy through complex claims resolution.<li>Wrote legal appeals on behalf of healthcare Providers & Patients.</li><li>Identified repetitive tasks, and wrote macros to automate them.</li><li>Conducted daily research on current insurance policies.</li>",
   },
   {
     title: "Licensee/Artist",
     cardImage: "assets/images/experience-page/pn.jpeg",
     place: "Paint Nite - Nashville & Memphis",
     time: "(2014 - 2019)",
-    desp:'The premier "social painting" company in the U.S.<li>Started as a Teaching Artist and took over operations for Nashville in 2017.</li><li>Grew Private Event revenue for the Nashville Market from roughly $10,000 annually to over $70,000 in my first year.</li><li>Launched Paint Nite Memphis in 2018, and developed self-sustaining team remotely from home in Nashville, TN.</li>',
+    desc:'The premier "social painting" company in the U.S.<li>Started as a Teaching Artist and took over operations for Nashville in 2017.</li><li>Grew Private Event revenue for the Nashville Market from roughly $10,000 annually to over $70,000 in my first year.</li><li>Launched Paint Nite Memphis in 2018, and developed self-sustaining team remotely from home in Nashville, TN.</li>',
   },
 ];
 
 const showCards2 = () => {
-  let output = "";
-  exp.forEach(
-    ({ title, cardImage, place, time, desp }) =>
-      (output += `
-      <ul>
-      <li class="card card1">
-        <img src="${cardImage}" class="featured-image"/>
-        <article class="card-body">
-          <header>
-            <div class="title">
-              <h3>${title}</h3>
-            </div>
-            <p class="meta">
-              <span class="pre-heading">${place}</span><br>
-              <span class="author">${time}</span>
-            </p>
-            <ol>
-              ${desp}
-            </ol>
-          </header>
-        </article>
-      </li>
-    </ul>
-      `)
-  );
-  experiencecards.innerHTML = output;
+    let output = "";
+    exp.forEach(e => {
+        output += `
+        <ul>
+            <li class="card card1">
+                <img src="${e.cardImage}" class="featured-image"/>
+                <article class="card-body">
+                <header>
+                    <div class="title">
+                        <h3>${e.title}</h3>
+                    </div>
+                    <p class="meta">
+                        <span class="pre-heading">${e.place}</span><br>
+                        <span class="author">${e.time}</span>
+                    </p>
+                    <ol>
+                    ${desc}
+                    </ol>
+                </header>
+                </article>
+            </li>
+        </ul>
+        `
+    });
+    experiencecards.innerHTML = output;
 };
 document.addEventListener("DOMContentLoaded", showCards2);
-
 
 // Volunteership Cards
 
